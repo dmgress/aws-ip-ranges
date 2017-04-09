ba246378d85d9fe2fcce289d7818bc48c3c4db90
Daniel Gressmann <dmgressmann@gmail.com>
2017-04-09 20:49:42 +0200

    Adding actual function content and latest template

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+let https = require('https');
+let AWS = require('aws-sdk');
+let s3 = new AWS.S3();
+
+exports.handler = (event, context, callback) => {
+  let message = event.Records[0].Sns.Message;
+  if (typeof message === 'string') {
+    message = JSON.parse(message);
+  }
+  let url = message.url || "https://ip-ranges.amazonaws.com/ip-ranges.json";
+  console.log('Going to fetch ' + url);
+  https.get(url, function (res) {
+    res.setEncoding('utf8');
+    var rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      let parsedData = JSON.parse(rawData);
+      //console.log(parsedData);
+      var params = {
+        "Bucket": process.env.S3Bucket,
+        "Key": `ipranges-${parsedData.syncToken}.json`,
+        "Body": rawData
+      };
+      s3.putObject(params, function(err, data) {
+        if (err) {
+          callback(err, err.stack); // an error occurred
+        } else {
+          callback(null,"success"); // successful response
+        }
+      });
+    });
+  });
+}
