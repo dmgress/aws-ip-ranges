f444628e699eb39e5f04bdd40a92457cffc2abc0
Daniel Gressmann <dmgressmann@gmail.com>
2022-04-25 07:03:54 +0000

    change to whatwg url api

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const AWSXRay = require('aws-xray-sdk');
-const urllib = require('url');
 const https = AWSXRay.captureHTTPs(require('https'));
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 const s3 = new AWS.S3();
@@ -27,7 +26,7 @@ exports.handler = (event, context, callback) => {
   }
   const url = message.url || "https://ip-ranges.amazonaws.com/ip-ranges.json";
   console.log('Going to fetch ' + url);
-  const options = urllib.parse(url);
+  const options = new URL(url);
   https.get(options, function (res) {
     res.setEncoding('utf8');
     let rawData = "";
