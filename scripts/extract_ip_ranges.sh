871f1e2d68117c0ccc75b588ff0ad8ad21b40b85
Daniel Gressmann <dmgressmann@gmail.com>
2026-08-02 21:24:36 +0200

    fix(process-ip-ranges): force empty full ip ranges json file

--- scripts/extract_ip_ranges.sh
+++ scripts/extract_ip_ranges.sh
@@ -3,13 +3,17 @@
 # Ensure you're in the correct git repo
 cd $(dirname $0)/.. || exit 1
 
+FULL_IP_RANGES_JSON="output/ip-ranges.full.json"
+
 # Create output directory if it doesn't exist
 mkdir -p output
+rm -rf $FULL_IP_RANGES_JSON
+touch $FULL_IP_RANGES_JSON
 
-# Get list of commits affecting ip-ranges.json, sorted oldest to newest
 git log --follow --format="%H" -- ip-ranges.json | while read commit; do
     # Get commit date as Unix timestamp
     timestamp=$(git show -s --format=%ct "$commit")
     # Checkout the file at this commit
-    git show "$commit":ip-ranges.json > "output/ip-ranges.$timestamp.json"
+    # git show "$commit":ip-ranges.json | tee "output/ip-ranges.$timestamp.json" | jq -c >> $FULL_IP_RANGES_JSON
+    git show "$commit":ip-ranges.json | jq -c >> $FULL_IP_RANGES_JSON
 done
